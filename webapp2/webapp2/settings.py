dd84fb351833bbb2fec26396e4e9f791e083c27b
aconcan <concannonaimee@gmail.com>
2021-05-08 11:08:56 +0100

    added static files and assets folder—styles.css and image

--- webapp2/webapp2/settings.py
+++ webapp2/webapp2/settings.py
@@ -126,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# Specify local static file location
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'assets'),
+)
--- webapp2/webapp2/urls.py
+++ webapp2/webapp2/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +10,6 @@ urlpatterns = [
     # Including the urls file from the articles app
     path('articles/', include('articles.urls')),
 ]
+
+# Appending method to urlpatterns so Django knows how to serve up static files
+urlpatterns += staticfiles_urlpatterns()
