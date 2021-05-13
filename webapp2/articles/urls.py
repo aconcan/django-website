1ea59919ebcf7d40c663d4e5255e1e4e69808674
aconcan <concannonaimee@gmail.com>
2021-05-13 10:28:47 +0100

    added named capturing group to urls.py for article slugs

--- webapp2/articles/urls.py
+++ webapp2/articles/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.article_list),
+    # Defining named capturing group and passing to article_detail as a parameter 
+    path('<slug:slug>/', views.article_detail),
 ]
