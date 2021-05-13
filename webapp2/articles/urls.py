3a00eb4ccd43e125584fa513a1e5021af13bbe4a
aconcan <concannonaimee@gmail.com>
2021-05-13 11:21:28 +0100

    added URL naming to article list URLs and dynamic hyperlines to article.title headings in article_list

--- webapp2/articles/urls.py
+++ webapp2/articles/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 
+# Namespacing urls file
+app_name = 'articles'
+
 urlpatterns = [
-    path('', views.article_list),
-    # Defining named capturing group and passing to article_detail as a parameter 
-    path('<slug:slug>/', views.article_detail),
+    path('', views.article_list, name='list'),
+    # Defining named capturing group and passing to article_detail as a parameter
+    path('<slug:slug>/', views.article_detail, name='detail'),
 ]
