c56e76a0929c777052281014ac137fc56a78e960
aconcan <concannonaimee@gmail.com>
2021-05-16 15:25:10 +0100

    added user login functionality (backend)

--- webapp2/accounts/views.py
+++ webapp2/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Fired every time a request hits the 'signup/' URL
 # Need to differentiate between POST and GET requests
@@ -8,8 +9,9 @@ def signup_view(request):
         # Sending data into the form for validation
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # REMINDER: log user in here
+            # Log user in
+            user = form.save()
+            login(request, user)
             
             # Redirect user to articles URL named list (article homepage)
             return redirect('articles:list')
@@ -26,7 +28,8 @@ def login_view(request):
         # Validation
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # REMINDER: Log the user in.
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
     
     # If GET    
