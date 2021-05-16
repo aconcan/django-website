f40832c96eff7212b8f9ea25d05820e799400772
aconcan <concannonaimee@gmail.com>
2021-05-16 15:10:00 +0100

    added front-end login screen for users

--- webapp2/accounts/views.py
+++ webapp2/accounts/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+# Fired every time a request hits the 'signup/' URL
+# Need to differentiate between POST and GET requests
 def signup_view(request):
-    form = UserCreationForm()
-    # Sending instance of the form to the template
+    if request.method == 'POST':
+        # Sending data into the form for validation
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # REMINDER: log user in here
+            
+            # Redirect user to articles URL named list (article homepage)
+            return redirect('articles:list')
+    
+    else:
+        form = UserCreationForm()
+    
+    # Send instance of form to the template
     return render(request, 'accounts/signup.html', { 'form': form })
+
+
+def login_view(request):
+    if request.method == 'POST':
+        # Validation
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # REMINDER: Log the user in.
+            return redirect('articles:list')
+    
+    # If GET    
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'accounts/login.html', { 'form': form })
 
