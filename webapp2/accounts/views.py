2ea60661b6634b137dab1658329d0b9c8ab1f2a9
aconcan <concannonaimee@gmail.com>
2021-05-17 10:47:34 +0100

    added create page redirect after login using the response.GET.next parameter

--- webapp2/accounts/views.py
+++ webapp2/accounts/views.py
@@ -30,7 +30,10 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('articles:list')
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else: 
+                return redirect('articles:list')
     
     # If GET    
     else:
